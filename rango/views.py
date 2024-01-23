6f3b097c4dd942f0c5cc37a425df30f4d20fcf21
Nathan Rooney <2658846r@student.gla.ac.uk>
2024-01-23 22:47:09 +0000

    most recent update

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("Rango says this is the about page! Here's a link back to the homepage <a href='/rango/'>Index</a>")
